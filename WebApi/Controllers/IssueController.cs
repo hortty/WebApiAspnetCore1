2be003ac69c722c301f1f7a745f19bdf93fa25f8
Gabriel Bueno <gabrielhortmann@gmail.com>
2022-11-29 23:24:21 -0300

    First Web API with Aspnet Core 6

---
+++ WebApi/Controllers/IssueController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+
+        public IssueController(IssueDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get()
+        {
+            return await _context.Issues.ToListAsync();
+        }
+
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            return issue == null ? NotFound() : Ok(issue);  
+        }
+
+
+    }
+}
